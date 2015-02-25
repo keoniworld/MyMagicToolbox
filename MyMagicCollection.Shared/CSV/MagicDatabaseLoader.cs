e0c83f6b26892a0affa06a48e7dfb17b4505234a
Sigurdur42 <MichaelWagner42@googlemail.com>
2015-02-25 21:59:20 +0100

    Some tests

--- MyMagicCollection.Shared/CSV/MagicDatabaseLoader.cs
+++ MyMagicCollection.Shared/CSV/MagicDatabaseLoader.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using CsvHelper;
+using MyMagicCollection.Shared.Helper;
 using MyMagicCollection.Shared.Models;
 
 namespace MyMagicCollection.Shared.CSV
@@ -11,30 +12,40 @@ namespace MyMagicCollection.Shared.CSV
         {
         }
 
-        public IEnumerable<MagicCardDefinition> LoadCardDatabase(FileInfo databaseFile)
+        public IEnumerable<MagicCardDefinition> LoadCardDatabase()
         {
-            var inputCsv = new CsvReader(new StringReader(File.ReadAllText(databaseFile.FullName)));
-
             var result = new List<MagicCardDefinition>();
-            while (inputCsv.Read())
+            var assembly = GetType().Assembly;
+            var resourceName = assembly.FindEmbeddedResource("CSV.MagicDatabase.csv");
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
             {
-                var line = inputCsv.GetRecord<MagicCardDefinition>();
-                result.Add(line);
+                var inputCsv = new CsvReader(new StreamReader(stream));
+
+                while (inputCsv.Read())
+                {
+                    var line = inputCsv.GetRecord<MagicCardDefinition>();
+                    result.Add(line);
+                }
             }
             return result;
         }
 
-        public IEnumerable<MagicSetDefinition> LoadSetDatabase(FileInfo databaseFile)
+        public IEnumerable<MagicSetDefinition> LoadSetDatabase()
         {
-            var inputCsv = new CsvReader(new StringReader(File.ReadAllText(databaseFile.FullName)));
-
             var result = new List<MagicSetDefinition>();
-            while (inputCsv.Read())
+            var assembly = GetType().Assembly;
+            var resourceName = assembly.FindEmbeddedResource("CSV.MagicDatabaseSets.csv");
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
             {
-                var line = inputCsv.GetRecord<MagicSetDefinition>();
-                result.Add(line);
+                var inputCsv = new CsvReader(new StreamReader(stream));
+
+                while (inputCsv.Read())
+                {
+                    var line = inputCsv.GetRecord<MagicSetDefinition>();
+                    result.Add(line);
+                }
+                return result;
             }
-            return result;
         }
     }
 }
---
+++ MyMagicCollection.Shared/Helper/EmbeddedResourceHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMagicCollection.Shared.Helper
+{
+    public static class EmbeddedResourceHelper
+    {
+        public static string FindEmbeddedResource(this Assembly assembly, string resourceName)
+        {
+            resourceName = resourceName.ToLowerInvariant();
+            return assembly.GetManifestResourceNames()
+                .FirstOrDefault(n => n.ToLowerInvariant().Contains(resourceName));
+        }
+    }
+}
